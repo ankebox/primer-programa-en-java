1ccc67ac8f390cf1ac992fc20049b8b065cfc9e3
David <davidaicardocossiopulido@gmail.com>
2020-10-01 19:03:41 +0200

    el nuevo cambio es que hemos añadido una linea con mi localidad de nacimiento y la fecha de nacimiento

--- Prueba.java
+++ Prueba.java
@@ -2,7 +2,7 @@ public class Prueba {
 	
 	public static void main(String args[]){
 		System.out.println("David Aicardo Cossio Pulido");
-		
+		System.out.println("naci en pereira en 2001");
 		
 		
 	}
