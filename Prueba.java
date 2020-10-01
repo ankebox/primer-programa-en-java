9eab8d3e9095412d120ac9f4330579e8928d3733
David <davidaicardocossiopulido@gmail.com>
2020-10-01 19:20:08 +0200

    el cambio que eh echo en este nuevo commit esq eh añadido una nueva linea de codigo que muestra mi localidad actual y el sistema operativo que uso

--- Prueba.java
+++ Prueba.java
@@ -3,7 +3,7 @@ public class Prueba {
 	public static void main(String args[]){
 		System.out.println("David Aicardo Cossio Pulido");
 		System.out.println("naci en pereira en 2001");
-		
+		System.out.println("Mi localidad actual es leon y uso el sistema operativo windows 10");
 		
 	}
 	
