37ff6848443d5ae2ae2fffe30f23fb837c61eff6
David <davidaicardocossiopulido@gmail.com>
2020-10-02 09:27:31 +0200

    ahora se añadio cuarta linea con nombre completo

--- Prueba.java
+++ Prueba.java
@@ -4,7 +4,7 @@ public class Prueba {
 		System.out.println("David Aicardo Cossio Pulido");
 		System.out.println("naci en pereira en 2001");
 		System.out.println("Mi localidad actual es leon y uso el sistema operativo windows 10");
-		
+		System.out.println("David Aicardo Cossio Pulido");
 	}
 	
 	
